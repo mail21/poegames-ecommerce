1b1ae89f19f62c99709d1d6da13588fc28525af4
mail21 <ismailidn1@gmail.com>
2020-10-13 20:04:55 +0700

    Edit Navbar

--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -2,19 +2,20 @@ import React from 'react';
 import Logo from './../../assets/poegameslogo.png';
 import LogoText from './../../assets/poegamestext.png';
 import CartIcon from './../CartIcon/CartIcon';
+import { Link } from 'react-router-dom';
 
 import './navbar.styles.scss';
 
 function Navbar() {
   return (
     <nav>
-      <div>
-        <img className="nav-logo" src={Logo} alt="logo awal" width="90px" />
-        <img src={LogoText} alt="text logo" width="350" />
-      </div>
-      <ul>
+      <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
+        <img className="nav-logo" src={Logo} alt="logo awal" width="80px" />
+        <img src={LogoText} alt="text logo" width="250" />
+      </Link>
+      <ul style={{ alignItems: 'center' }}>
         <li>
-          <a href="#">Products</a>
+          <Link to="/products">Products</Link>
         </li>
         <li>
           <a href="#">Checkout</a>
