7dd24e207751018f466fc4f0378d5595dbbbbe02
mail21 <ismailidn1@gmail.com>
2020-10-16 19:40:59 +0700

    Add Landing Page component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './component/navbar/Navbar';
-
+import LandingPage from './pages/LandingPage/LandingPage';
 import ProductsPage from './pages/ProductsPage/ProductsPage';
 import ProductPage from './pages/ProductPage/ProductPage';
 import DaftarPage from './pages/DaftarPage/DaftarPage';
@@ -14,7 +14,7 @@ function App() {
       <Navbar />
 
       <Switch>
-        <Route exact path="/" component={ProductsPage} />
+        <Route exact path="/" component={LandingPage} />
         <Route exact path="/products" component={ProductsPage} />
         <Route path="/login" component={LoginPage} />
         <Route path="/signup" component={DaftarPage} />
---
+++ src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,84 @@
+import React, { useState, useContext, useEffect } from 'react';
+import './LandingPage.scss';
+import HomeSlideShow from './../../component/HomeSlideShow/HomeSlideShow';
+import LandingCards from './../../component/LandingCards/LandingCards';
+import LandingCardVertical from './../../component/LandingCardVertical/LandingCardVertical';
+import axios from 'axios';
+import { Context } from './../../context-api/context';
+import PacmanLoader from 'react-spinners/PacmanLoader';
+
+function LandingPage() {
+  const [{ headersAPI, API_URL }] = useContext(Context);
+  const [data, setData] = useState([]);
+  const [slideData, setSlideData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    let arr = [];
+    while (arr.length < 3) {
+      var r = Math.floor(Math.random() * Math.floor(11));
+      if (arr.indexOf(r) === -1) arr.push(r);
+    }
+    axios({
+      method: 'GET',
+      headers: headersAPI,
+      url: `${API_URL}/games`,
+      params: {
+        ordering: '-added',
+        dates: '2010-01-01,2020-09-29',
+        parent_platforms: '2,3',
+        page_size: 11,
+      },
+    })
+      .then((res) => {
+        setSlideData(() => {
+          return [
+            res.data.results[arr[0]],
+            res.data.results[arr[1]],
+            res.data.results[arr[2]],
+          ];
+        });
+        setData(res.data.results);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <div className="container__landing">
+      {isLoading ? (
+        <div className="container__loader__landing">
+          <PacmanLoader size={50} color={'yellow'} />
+        </div>
+      ) : (
+        <>
+          <HomeSlideShow dataSlide={slideData} />
+          <main className="container__main__landing">
+            <div className="container__cards__landing">
+              <h3>New releases</h3>
+              <LandingCards />
+              <h3>Highest Rated Game On PlayStation</h3>
+              <LandingCards />
+              <h3>Most Added Games On Ubisoft</h3>
+              <LandingCards />
+            </div>
+            <div className="container__sideCards">
+              <h3>Most Added games</h3>
+
+              {data.slice(0, 6).map((el) => (
+                <LandingCardVertical
+                  name={el.name}
+                  add={el.added}
+                  image={el.background_image}
+                  slug={el.slug}
+                />
+              ))}
+            </div>
+          </main>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default LandingPage;
