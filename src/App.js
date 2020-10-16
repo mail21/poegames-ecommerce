6a40d16a43dc8bf73412da84e1824dc9692aa7a9
mail21 <ismailidn1@gmail.com>
2020-10-16 13:31:48 +0700

    Add Route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import './App.css';
-import ProductsPage from './pages/ProductsPage';
 import Navbar from './component/navbar/Navbar';
 
+import ProductsPage from './pages/ProductsPage/ProductsPage';
 import ProductPage from './pages/ProductPage/ProductPage';
+import DaftarPage from './pages/DaftarPage/DaftarPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
 
       <Switch>
         <Route exact path="/" component={ProductsPage} />
         <Route exact path="/products" component={ProductsPage} />
         <Route path="/login" component={LoginPage} />
-        <Route path="/daftar" component={<></>} />
+        <Route path="/daftar" component={DaftarPage} />
         <Route path="/products/:slug" component={ProductPage} />
       </Switch>
-    </div>
+    </>
   );
 }
 
