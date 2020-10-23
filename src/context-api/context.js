56c51de97afeb66b0aae10fb3d3adb9dae1095a8
mail21 <ismailidn1@gmail.com>
2020-10-23 14:41:22 +0700

    Add useContextValue

--- src/context-api/context.js
+++ src/context-api/context.js
@@ -1,7 +1,9 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useContext } from 'react';
 
 export const Context = createContext();
 
 export const StateProvider = ({ reducer, initialState, children }) => (
   <Context.Provider value={useReducer(reducer, initialState)}>{children}</Context.Provider>
 );
+
+export const useContextValue = () => useContext(Context);
